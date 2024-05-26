b9c84cad2e457a961ab3c62498948640d4413aa1
Ranulfo Raphael <raanulfo@hotmail.com>
2024-05-26 14:44:07 -0300

    nvim: fix mouse and undodir

--- nvim/lua/rnlf/set.lua
+++ nvim/lua/rnlf/set.lua
@@ -14,7 +14,7 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.nvim/undodir"
+vim.opt.undodir = os.getenv("HOME") .. "/.config/nvim/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = true
@@ -26,7 +26,7 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
-vim.opt.mouse = a
+vim.opt.mouse =''
 
 vim.opt.signcolumn = "no"
 
