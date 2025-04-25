6616414cbd70902fa01966c0fa5f661115052778
Ranulfo Raphael <raanulfo@hotmail.com>
2025-04-25 09:07:38 -0300

    update: gitsigns

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -6,5 +6,10 @@ return {
         border = "rounded",
       }
     })
+    vim.keymap.set("n", "<leader>gs", ":Gitsigns preview_hunk <CR>")
+    vim.keymap.set("n", "<leader>gn", ":Gitsigns next_hunk <CR>")
+    vim.keymap.set("n", "<leader>gp", ":Gitsigns prev_hunk <CR>")
+    vim.keymap.set("n", "<leader>gr", ":Gitsigns reset_hunk <CR>")
+    vim.keymap.set("n", "<leader>gb", ":Gitsigns blame <CR>")
   end
 }
