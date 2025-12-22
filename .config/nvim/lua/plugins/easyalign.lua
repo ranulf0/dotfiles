aca918a66c8c4ac091956a667a7ef23e428345d1
Ranulfo Raphael <ranulfo.raphael@outlook.com>
2025-12-22 14:19:11 -0300

    update: nvim easy align

--- .config/nvim/lua/plugins/easyalign.lua
+++ .config/nvim/lua/plugins/easyalign.lua
@@ -2,7 +2,8 @@ return {
   "junegunn/vim-easyalign",
   lazy = false,
   keys = {
-    { "ga", "<Plug>(EasyAlign)", mode = { "n", "x" }, desc = "EasyAlign" },
+    { "ga", "<Plug>(EasyAlign)", mode = { "n", "x" }},
+    { "gl", "<Plug>(LiveEasyAlign)", mode = { "n", "x" }},
   },
   url = "git@github.com:junegunn/vim-easy-align.git"
 }
