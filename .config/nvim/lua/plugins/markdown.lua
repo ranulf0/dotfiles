fac057c276b18f7cc6053babc532950454035de1
Ranulfo Raphael <ranulfo.raphael@outlook.com>
2025-12-23 10:34:27 -0300

    update: update nvim markdown

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,10 +1,7 @@
 return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = function()
-    vim.fn.system({ "git", "reset", "--hard" })
-    vim.fn.system({ "cd app && npm install" })
-  end,
+  build = "cd app && npm install",
   init = function()
     vim.g.mkdp_filetypes = { "markdown" }
   end,
