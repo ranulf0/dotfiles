091c97f0693cd4f6e11d6ccee7dda7f2f5e1edbc
Ranulfo Raphael <ranulfo.raphael@outlook.com>
2026-01-17 09:54:07 -0300

    update: indent blankline

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -2,6 +2,13 @@ return {
   "lukas-reineke/indent-blankline.nvim",
   main = "ibl",
   opts = {
-    scope = { enabled = false },
+    scope = {
+      enabled = true,
+      show_start = false,
+      show_end = false
+    },
+    indent = {
+      char = "│",
+    },
   },
 }
