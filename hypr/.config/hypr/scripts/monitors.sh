a16b1f9cf6baf93172f80e98af21423cb6b51598
Ranulfo Raphael <ranulfo.raphael@outlook.com>
2026-04-06 11:03:34 -0300

    hypr: update monitor script params

--- hypr/.config/hypr/scripts/monitors.sh
+++ hypr/.config/hypr/scripts/monitors.sh
@@ -2,18 +2,18 @@
 
 MODE="$1"
 
-HDMI=$(hyprctl monitors all | awk '/HDMI-A-/{print $2; exit}')
+EXTERNAL=$(hyprctl monitors all | awk '/HDMI-A-/{print $2; exit}')
 INTERNAL=$(hyprctl monitors all | awk '/eDP|LVDS/ {print $2; exit}')
 
 if [ "$MODE" = "internal" ]; then
     [ -n "$INTERNAL" ] && hyprctl keyword monitor "$INTERNAL,preferred,0x0,1"
-    [ -n "$HDMI" ] && hyprctl keyword monitor "$HDMI,disable"
+    [ -n "$EXTERNAL" ] && hyprctl keyword monitor "$EXTERNAL,disable"
     exit 0
 fi
 
-if [ "$MODE" = "hdmi" ]; then
-    if [ -n "$HDMI" ]; then
-        hyprctl keyword monitor "$HDMI,preferred,0x0,1"
+if [ "$MODE" = "external" ]; then
+    if [ -n "$EXTERNAL" ]; then
+        hyprctl keyword monitor "$EXTERNAL,preferred,0x0,1"
         hyprctl keyword monitor "$INTERNAL,disable"
     else
         hyprctl keyword monitor "$INTERNAL,preferred,0x0,1"
@@ -21,5 +21,5 @@ if [ "$MODE" = "hdmi" ]; then
     exit 0
 fi
 
-[ -n "$HDMI" ] && hyprctl keyword monitor "$HDMI,preferred,0x0,1"
+[ -n "$EXTERNAL" ] && hyprctl keyword monitor "$EXTERNAL,preferred,0x0,1"
 [ -n "$INTERNAL" ] && hyprctl keyword monitor "$INTERNAL,preferred,0x0,1"
