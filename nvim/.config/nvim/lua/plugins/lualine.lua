14cc8e55263fd773ede4ff9e51841d795116986e
Ranulfo Raphael <ranulfo.raphael@outlook.com>
2026-05-07 08:27:06 -0300

    nvim: disable status line on alpha

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -37,7 +37,7 @@ return {
         component_separators = '|',
         section_separators = { left = '', right = '' },
         disabled_filetypes = {
-          statusline = { "NvimTree" },
+          statusline = { "NvimTree", "alpha" },
         },
       },
       sections = {
